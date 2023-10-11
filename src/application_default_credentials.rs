5aee9eb1f5c03133d1f9509cb7695050a353bb7f
Joe Neeman <joe.neeman@tweag.io>
2023-10-11 08:22:40 -0500

    Add id token auth for the metadata server

--- src/application_default_credentials.rs
+++ src/application_default_credentials.rs
@@ -1,8 +1,8 @@
 use crate::error::Error;
 use crate::types::TokenInfo;
+use http::Uri;
 use hyper::client::connect::Connection;
 use std::error::Error as StdError;
-use http::Uri;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tower_service::Service;
 
@@ -11,16 +11,28 @@ use tower_service::Service;
 pub struct ApplicationDefaultCredentialsFlowOpts {
     /// Used as base to build the url during token request from GCP metadata server
     pub metadata_url: Option<String>,
+    /// If true, asks for an ID token instead of an OAuth access token.
+    pub id_token: bool,
 }
 
 pub struct ApplicationDefaultCredentialsFlow {
     metadata_url: String,
+    id_token: bool,
 }
 
 impl ApplicationDefaultCredentialsFlow {
     pub(crate) fn new(opts: ApplicationDefaultCredentialsFlowOpts) -> Self {
-        let metadata_url = opts.metadata_url.unwrap_or_else(|| "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token".to_string());
-        ApplicationDefaultCredentialsFlow { metadata_url }
+        let id_token = opts.id_token;
+        let metadata_url = opts.metadata_url
+            .unwrap_or_else(|| if id_token {
+                 "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token".to_string()
+            } else {
+                 "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity".to_string()
+            });
+        ApplicationDefaultCredentialsFlow {
+            metadata_url,
+            id_token,
+        }
     }
 
     pub(crate) async fn token<S, T>(
@@ -36,7 +48,14 @@ impl ApplicationDefaultCredentialsFlow {
         S::Error: Into<Box<dyn StdError + Send + Sync>>,
     {
         let scope = crate::helper::join(scopes, ",");
-        let token_uri = format!("{}?scopes={}", self.metadata_url, scope);
+        let token_uri = format!(
+            "{}?{}={}",
+            self.metadata_url,
+            // For ID tokens we use the scope arguments to pass the audience. Bit of a hack, since
+            // this was originally designed only for the access tokens.
+            if self.id_token { "audience" } else { "scopes" },
+            scope
+        );
         let request = hyper::Request::get(token_uri)
             .header("Metadata-Flavor", "Google")
             .body(hyper::Body::from(String::new())) // why body is needed?
